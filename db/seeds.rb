661b53772ac52d788d4c292915b5d0afb8a3144c
Ricardo Piras <ricpiras@gmail.com>
2018-11-07 13:58:11 +0900

    Created routes, models with methods, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Joel Robucho',
+    address:      'Ebisu, Tokyo',
+    category:  'french',
+    #rating:        5
+  },
+  {
+    name:         'Rigoletto',
+    address:      'Roppongi, Tokyo',
+    category:  'italian',
+    #rating:        4
+  },
+  {
+    name:         'Antonio',
+    address:      'Nishi-Azabu, Tokyo',
+    category:  'italian',
+    #rating:        5
+  },
+  {
+    name:         'Pink Elephant',
+    address:      'Akasa Mitsuke, Tokyo',
+    category:  'belgian',
+    #rating:        3
+  },
+  {
+    name:         'Genki Sushi',
+    address:      'Shibuya, Tokyo',
+    category:  'japanese',
+    #rating:        3
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
